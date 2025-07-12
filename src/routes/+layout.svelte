a1490faf5b3d89080f52bc2d0d9c2f5ac4834e40
Eduardo <dev_eduardo@hotmail.com>
2025-07-12 17:46:10 -0300

    fix: move mode script to layout head (#56)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,18 @@
   <Navbar />
   {@render children()}
 </div>
+
+<svelte:head>
+  <title>Sveltekit Travel Log</title>
+  <meta
+    name="description"
+    content="Keep track of your travels and adventures. Add locations, photos, and notes to create a digital journal of your journeys"
+  />
+
+  <script>
+    const mode = localStorage.getItem("mode") ?? "dark";
+
+    document.documentElement.dataset.mode = mode;
+    localStorage.setItem("mode", mode);
+  </script>
+</svelte:head>
