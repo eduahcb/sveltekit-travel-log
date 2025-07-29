24b27095f3a205f265beea9f536b118c2a7b3498
Eduardo <dev_eduardo@hotmail.com>
2025-07-29 20:52:36 -0300

    feat: add sidebar navigation layout (#64)
    Introduce a new dashboard layout with a sidebar navigation

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { Navigation } from "@skeletonlabs/skeleton-svelte";
+
+  type NavItemProps = {
+    label: string;
+    href: string;
+    children: Snippet;
+  };
+
+  const { href, children, label }: NavItemProps = $props();
+</script>
+
+<Navigation.Tile aspect="auto" labelExpanded={label} {href}>
+  {@render children()}
+</Navigation.Tile>
---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import NavItem from "$lib/components/NavItem.svelte";
+
+  import { LogOut, Map, Menu, Plus } from "@lucide/svelte";
+  import { Navigation } from "@skeletonlabs/skeleton-svelte";
+
+  const { children } = $props();
+
+  let isExpansed = $state(true);
+
+  function toggleExpanded() {
+    isExpansed = !isExpansed;
+  }
+</script>
+
+<main class="flex-1 flex gap-4">
+  <div>
+    <Navigation.Rail
+      expanded={isExpansed}
+      base="flex flex-col gap-5"
+      tilesBase="flex flex-col border-b-1 pb-2"
+      tilesGap="gap-3"
+    >
+      {#snippet header()}
+        <Navigation.Tile
+          onclick={toggleExpanded}
+          title="Toggle Menu Width"
+          aspect="auto"
+        >
+          <Menu />
+        </Navigation.Tile>
+      {/snippet}
+
+      {#snippet tiles()}
+        <NavItem href="#" label="Locations">
+          <Map />
+        </NavItem>
+
+        <NavItem href="#" label="Add Location">
+          <Plus />
+        </NavItem>
+      {/snippet}
+
+      {#snippet footer()}
+        <NavItem href="#" label="Sign Out">
+          <LogOut />
+        </NavItem>
+      {/snippet}
+    </Navigation.Rail>
+  </div>
+  <div class="flex-1">
+    {@render children()}
+  </div>
+</main>
