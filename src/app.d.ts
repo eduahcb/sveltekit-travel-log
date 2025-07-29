4301fabd76a7a5e17728205ab4a353e94f4aa1c2
Eduardo <dev_eduardo@hotmail.com>
2025-07-29 18:33:08 -0300

    refator: move session to event.locals (#63)
    Move session retrieval to `event.locals` in hooks and update layout server load to use `locals.session.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,19 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+//
+
+import type { Session } from "./lib/types";
+
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      session: Session;
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
   }
 }
 
-export {};
+export { };
