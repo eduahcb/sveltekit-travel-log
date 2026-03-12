eedce8aa358b840deed71328a05a161aafa603db
Eduardo <dev_eduardo@hotmail.com>
2026-03-12 18:44:18 -0300

    feat: add integrated form for user input in form-integration page (#76)
    Created a new Svelte component for a form that collects user information including email, name, position, phone, age, and color preferences. This enhances user interaction and data collection capabilities.

---
+++ src/routes/form-integration/+page.svelte
@@ -0,0 +1,51 @@
+<form class="form" name="new-form-integration" id="fnew-form-integration">
+    <h1>Página de Form Integrado</h1>
+
+    <div class="mb-3">
+      <label>E-mail</label>
+      <input id="email" name="email" class="form-control" />
+    </div>
+
+    <div class="mb-3">
+      <label>Nome</label>
+      <input id="name" name="name" class="form-control" />
+    </div>
+
+    <div class="mb-3">
+      <label>Cargo</label>
+      <input id="cargo" name="cargo" class="form-control" />
+    </div>
+
+    <div class="mb-3">
+      <label>Telefone</label>
+      <input
+        type="tel"
+        id="telefone"
+        name="telefone"
+        class="form-control"
+      />
+    </div>
+
+    <div class="mb-3">
+      <label>Idade</label>
+      <input id="cf_idade" name="cf_idade" class="form-control" />
+    </div>
+
+  
+
+<div class="mb-3">
+       <select name="colors" id="colors" multiple>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="yellow">Yellow</option>
+        <option value="purple">Purple</option>
+    </select>
+    </div>
+
+    <div class="mb-3">
+      <button class="btn btn-primary my-4" type="submit">Enviar</button>
+    </div>
+  </form>
+
+  <script type="text/javascript" async src="https://d2umgtt5tsqx05.cloudfront.net/js/loader-scripts/f73b3f18-c649-4494-b241-db05ea04ca27-loader.js" ></script> 
